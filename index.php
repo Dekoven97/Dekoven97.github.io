768e0c7ccd7d263643d2cd3ad814b9b82e2ea37a
Dekoven97 <jamesdekoven43@gmail.com>
2022-04-08 09:56:40 -0400

    first try

---
+++ index.php
@@ -0,0 +1,381 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>De koven</title>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="shortcut icon" href="assets/img/ico-01.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+    <!--for Hover-->
+    <link rel="stylesheet" href="css/Hover-master/css/hover.css">
+    <link rel="stylesheet" href="css/Hover-master/css/hover.min.css">
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+   
+
+
+
+
+
+</head>
+<body>
+
+<!-------------------------------Navbar and Header--------------------------->
+
+  
+<div class="header" id="home">
+     
+     
+
+      <nav class="navbar navbar-expand-lg   navbar-dark ">
+             <div class="container-fluid">
+                 <a class="navbar-brand" href="#">De Koven</a>
+                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                     <span class="navbar-toggler-icon"></span>
+                 </button>
+       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav ms-auto ">
+                   <a class="nav-link active" aria-current="page" href="#">Home</a>
+                   <a class="nav-link   js-scroll-trigger" href="#portfolio port">Portfolio</a>
+                    <a class="nav-link   js-scroll-trigger" href="#about">About</a>
+                    <a class="nav-link   js-scroll-trigger" href="#Articles">Articles</a>
+               </div>
+         </div>
+         </div>
+      </nav>
+
+
+        <div class="row container title ms-md-5" >
+
+             <div class=" col-md-8 d-flex flex-column">
+   
+                   <div class="display-4 text-light ">James De Koven PIERRE</div>
+                   <small class="text-light ">Programmer-Junior Designer-Business manager</small>
+                   <p class="text-light text-muted" style="font-size: 0.8rem;">IT consultant at INUFOCAD | Web Developper at OMEGA-PC | Junior Designer at Tainos Visual Arts</p>
+                 <a class="btn btn-light cv" href="assets/CV.pdf" download >Download CV</a>
+                
+                </div>
+
+ 
+        </div>
+      
+</div>
+
+
+
+<!---------------------------------------------------------------------------------------->
+
+
+
+
+<!-------------------------------Portfolio--------------------------->
+     
+ <section id="portfolio port">
+
+<div class="container">
+
+
+
+<!------------------------------title--------------------------------->
+
+<div class="display-6 d-flex justify-content-center mt-5">Portfolio</div>
+       <hr class="w-50 mx-auto">
+
+
+
+ 
+ <!----------------------------------logo-1----------------------------->
+<div class="row justify-content-center log ">
+         <div class="col-md-4">
+         <a href="https://tainosvisualarts.com/" class="hvr-bounce-in omega"><img src="assets/img/img3.png"       class="img-fluid"alt=""></a>
+
+        
+
+
+
+
+         </div>
+
+
+         <div class="col-md-4">
+           <a href="https://tainosvisualarts.com/ayitiquest/"  class="hvr-bounce-in" > <img src="assets/img/img2.png"  class="img-fluid"alt=""></a>
+         </div>
+
+
+           <div class="col-md-4">
+            <a href="https://www.inufocad.edu.ht/"  class="hvr-bounce-in" ><img src="assets/img/img1.png"  class="img-fluid"alt=""></a> 
+          </div>
+
+
+</div>
+
+
+
+
+
+
+
+ 
+
+
+<!---------------------------------------logo 2------------------------------------->
+
+<div class="row justify-content-center mt log2">
+      <div class="col-md-4">
+       <a   class="hvr-bounce-in   isDisabled"><img src="assets/img/img5.png"    class="img-fluid"alt=""></a>
+      </div>
+
+
+      <div class="col-md-4">
+         <a href="http://zentoupatou.com/"  class="hvr-bounce-in" > <img src="assets/img/img6.png"  class="img-fluid"alt=""></a>
+      </div>
+
+
+        <div class="col-md-4">
+          <a   class="hvr-bounce-in   isDisabled" ><img src="assets/img/img4.png"  class="img-fluid"alt=""></a> 
+        </div>
+
+
+</div>
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+      </section>
+
+
+
+
+
+
+
+      
+<section id="about">
+
+
+
+<div class="container">
+
+
+
+<div class="display-6 d-flex justify-content-center abtitle">About</div>
+       <hr class="w-50 mx-auto">
+
+
+
+
+
+
+
+
+<div class="row featurette">
+        <div class="col-md-7 order-2  align-self-center">
+          <h2 class="featurette-heading">James De Koven PIERRE <span class="text-muted"></span></h2>
+          <p class="lead">Holder of a diploma in management and a degree in computer science from <a href="https://www.esih.edu/"  style=" text-decoration: none;  color: #1E2F97; font-weight:bold;" >ESIH</a>, I am passionate about technology especially in Web development, designing, 3d and VR/AR
+I am always eager for new knowledge and I adapt quickly</p>
+        </div>
+        <div class="col-md-5 order-1 pic ">
+         <img src="assets/img/img11.png"  alt="" class="img-fluid pics ">
+            <title>Placeholder</title>
+          
+        
+
+        </div>
+      </div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</section>
+
+
+
+
+
+<section id="Articles">
+  <div class="container">    
+
+
+  <div class="display-6 d-flex justify-content-center abtitle">Articles</div>
+       <hr class="w-50 mx-auto">
+
+
+
+
+
+
+       <div class="cards">
+
+
+       <a href="https://lenouvelliste.com/article/226924/le-projet-maac-pour-une-meilleure-decouverte-et-valorisation-des-artistes-afro-americano-caribeennes"  class="hvr-bounce-in" > 
+        <div class="card ">
+            <div class="containers">
+                <img src="https://pbs.twimg.com/media/Evw43stWYAE4eTj?format=jpg&name=small" alt="las vegas">
+            </div>
+            <div class="details">
+                <h3>MAAC</h3>
+                <p>Le projet MAAC : pour une meilleure découverte et valorisation des artistes afro-américano-caribéennes</p>
+            </div>
+        </div>
+       </a>
+
+
+       <a href="https://learndigital.withgoogle.com/digitalgarage/validate-certificate-code"  class="hvr-bounce-in" > 
+
+        <div class="card ">
+            <div class="containers">
+                <img src="https://pbs.twimg.com/media/EVcQq65XkAEZDnY?format=jpg&name=medium" alt="New York">
+            </div>
+            <div class="details">
+                <h3>Digital Marketing</h3>
+                <p>Fundamental of Digital Marketing Certification </br>Certificate ID: FPC NEQ 7K4</p>
+            </div>
+        </div>
+       </a>
+
+
+       <a href="https://web.facebook.com/permalink.php?story_fbid=10157768030084281&id=131705144280""  class="hvr-bounce-in" > 
+        <div class="card ">
+            <div class="containers">
+                <img src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.6435-9/75594438_10157768027549281_6789582278310756352_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeFJ4eFAA95VqHBadnNJXGmhiFPrXLACYzuIU-tcsAJjO1TxkxksNgPTIWNj6AfxVWp9hnXJAhjlheYROltAY-2w&_nc_ohc=B_byxXBHgg4AX_3z1Gi&tn=OTkpg1EG6acVtjJM&_nc_ht=scontent-iad3-1.xx&oh=00_AT-hhFI8OKbLi4pxpR9TORBV7vDTa5zxrStn8H3B6XjD3g&oe=624411B9" alt="">
+            </div>
+            <div class="details">
+                <h3> English Conversation Club</h3>
+                <p>workshop about EducationUSA and UGrad</p>
+            </div>
+        </div>
+       </a>
+
+
+       <a href="https://bustekmedia.com/dev-expo-2021-4-startups-haitiennes-levent-28000-usd-dinvestissement/"  class="hvr-bounce-in" > 
+        <div class="card ">
+            <div class="containers">
+                <img src="assets/img/devexpo (1).jpg" alt="">
+            </div>
+            <div class="details">
+                <h3>DevExpo</h3>
+                <p>En route vers la Cariverse,AyitiQuest</p>
+            </div>
+        </div>
+       </a>
+
+
+    </div>
+
+
+
+</div>
+</section>
+
+
+
+
+<section id="footer mt-auto" class="bg-light">
+
+<div class="container">
+
+<footer class="mt-2 ">
+    <div class="row text-center fo ">
+        <div class=" col-md-3"></div>
+        <div class="  col-md-6 link">
+                          <div class=" mb-3">For Contact </div>
+                        <a class="btn btn-social      mx-1" href="https://www.facebook.com/pierre.jamesdekoven"><i class="fab fa-fw fa-facebook-f"></i></a>
+                        <a class="btn btn-social     mx-1" href="https://twitter.com/DeKoven97"><i class="fab fa-fw fa-twitter"></i></a>
+                        <a class="btn btn-social     mx-1" href="https://www.linkedin.com/in/james-de-koven-pierre-a515b1191"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                        <a class="btn btn-social     mx-1" href="https://github.com/Dekoven97"><i class="fab fa-fw fa-github"></i></a>
+                        <a class="btn btn-social     mx-1" href="mailto:jamesdekoven43@gmail.com"><i class="fas fa-envelope"></i></a>
+
+        </div>
+        <div class=" col-md-3 "></div>
+    </div>
+
+</footer>
+ 
+</div>
+
+
+
+
+<a class="d-flex justify-content-end text-muted me-5 copy" style="text-decoration: none;">&copy; 2022 Dekoven</a>
+
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--------------- Paste it before </body> tag ------------->
+
+<script type="text/javascript">    
+           $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+      </script>
+
+<!---- Copy and paste it on anchor tag class=”js-scroll-trigger” as show in video ---->
+
+</body>
+</html>
